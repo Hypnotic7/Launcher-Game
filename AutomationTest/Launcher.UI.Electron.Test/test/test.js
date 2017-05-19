21d601ad9105e5a16922fadb9be517dbc3d69a91
MVProduction <wrzosekm71@gmail.com>
2017-05-19 11:15:25 +0100

    MVLauncher::UI::Test::Set up Spectron framework to test UI Electron_Make first automated test

--- AutomationTest/Launcher.UI.Electron.Test/test/test.js
+++ AutomationTest/Launcher.UI.Electron.Test/test/test.js
@@ -1,26 +1,27 @@
-﻿var describe = require('mocha').describe;
+﻿var Application = require('spectron').Application
+var assert = require('assert')
 
-var assert = require('assert');
+describe('application launch', function () {
 
-var Application = require('spectron').Application;
+    beforeEach(function () {
+        this.app = new Application({
+            path: '../../Launcher.UI.Electron/node_modules/electron/dist/electron.exe'
+        })
+        return this.app.start()
+    })
 
-describe('First Test',
-    function() {
-        this.timeout(10000);
-        beforeEach(function() {
-            this.app = new Application({ path: '../../Launcher.UI.Electron/app.js' });
-            return this.app.start();
-        });
-        afterEach(function() {
-            if (this.app && this.app.isRunning()) {
-                return this.app.stop();
-            }
-        });
-        it('showWindow',
-            function() {
-               assert.equal(1,1);
-            });
-    });
+    afterEach(function () {
+        if (this.app && this.app.isRunning()) {
+            return this.app.stop()
+        }
+    })
+
+    it('shows an initial window', function () {
+        return this.app.client.getWindowCount().then(function (count) {
+            assert.equal(count, 1)
+        })
+    })
+})
 
 
 
