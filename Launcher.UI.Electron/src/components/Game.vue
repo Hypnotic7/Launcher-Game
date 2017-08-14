b79d458b85725a99f40e6b9f890a6a16eddbe97d
MVProduction <wrzosekm71@gmail.com>
2017-08-14 16:40:09 +0100

    Game part2

--- Launcher.UI.Electron/src/components/Game.vue
+++ Launcher.UI.Electron/src/components/Game.vue
@@ -1,41 +1,61 @@
 ﻿<template>
   <div class="game-component">
-    <div class="game-nav">
-      <div class="row">
-        <div class="col-md-4">
-          <button type="button" id="game1_button" name="game1_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
-            <span class="glyphicon 	glyphicon glyphicon-screenshot" aria-hidden="true"> Game 1</span>
-          </button>
+    <div class="game-nav">      
+        <div class="row">
+          <div class="col-md-4">
+            <button type="button" id="game1_button" name="game1_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
+              <span class="glyphicon 	glyphicon glyphicon-screenshot" aria-hidden="true"> Game 1</span>
+            </button>
+          </div>
+          <div class="col-md-4">
+            <button type="button" id="game2_button" name="game2_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
+              <span class="glyphicon glyphicon glyphicon-fire" aria-hidden="true"> Game 2</span>
+            </button>
+          </div>
+          <div class="col-md-4">
+            <button type="button" id="game3_button" name="game3_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
+              <span class="glyphicon glyphicon glyphicon-tower" aria-hidden="true"> Game 3</span>
+            </button>
+          </div>
         </div>
-        <div class="col-md-4">
-          <button type="button" id="game2_button" name="game2_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
-            <span class="glyphicon glyphicon glyphicon-fire" aria-hidden="true"> Game 2</span>
-          </button>
-        </div>
-        <div class="col-md-4">
-          <button type="button" id="game3_button" name="game3_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
-            <span class="glyphicon glyphicon glyphicon-tower" aria-hidden="true"> Game 3</span>
-          </button>
-        </div>
-      </div>
     </div>
     
-      <div class="row">
-        <div class="col-lg-4">
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-5">
+        <div class="news">
           <div class="square-service-block">
-            <button type="button" id="game3_button" name="game3_button" class="btn btn-primary btn-lg btn-block" v-on:click="OnGameClicked">
-              <div class="glyphicon glyphicon glyphicon-tower"><i class="fa fa-paint-brush" aria-hidden="true"></i></div>
-              <h2 class="ssb-title">Install</h2>
-            </button>
+            <h2 class="ssb-title">News</h2><br><br><br><br><br><br>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="latest-release">
+          <div class="square-service-block">
+            <h2 class="ssb-title">Latest Release</h2><br><br><br><br><br><br>
+          </div>
+        </div>
+          <div class="col-md-1"></div>
+        </div>
+     </div>
+
+    <div class="row">
+      <div class="install-update">
+        <div class="col-md-3">
+          <div class="install-update-buttons">  
+              <button type="button" id="install_button" name="install_button" class="btn btn-primary btn-lg" v-on:click="OnInstallClicked">
+            <div class="square-service-block">Install</div><br></button>
           </div>
         </div>
         <div class="col-md-8">
           <div class="progress">
-            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">100%
-            </div>
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">100%</div>            
           </div>
+          <div class="col-md-12"><span id="latest_version_label" aria-hidden="true">Latest Version: v1.0.0</span></div>
         </div>
+        <div class="col-md-1"></div>
       </div>
+    </div>
 
       <nav class="navbar navbar-fixed-bottom">
         <div class="navbar-header">
@@ -111,6 +131,7 @@
 
   #game1_button, #game2_button, #game3_button {
     border: 1px solid white;
+    width: 100%;
   }
 
   #back_to_main_menu_button{
@@ -120,5 +141,37 @@
 
   #back_to_main_menu_button, #logout_button {
     margin-right: 1.25em;
+  }
+
+  .news, .latest-release {
+    background-color: #106485;
+    background: url("https://media1.giphy.com/media/CzuF1slv4dKxi/200_s.gif") no-repeat 50% 50%;
+    border: 1px solid white;
+    margin-top: 6em;
+  }
+
+  .install-update-buttons {
+    text-align:center;
+  }
+  
+  .install-update {
+    margin-top: 4em;
+    width:100%;
+  }
+
+  #install_button {
+    width: 100%;
+    
+  }
+
+  .square-service-block{
+    text-align: center;
+    text-size-adjust: 20%;
+  }
+  #latest_version_label {
+    color:white;    
+    top: 1em;
+    float: left;
+    right: 3em;
   }
 </style>
